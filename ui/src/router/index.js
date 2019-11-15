1ab169dfcae57b66e719cd6ecbec913fdecb7f83
Joshua Mathianas <mathianasj@idsysapps.com>
2019-11-15 13:50:14 -0500

    adding ui auth integration updates

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import VueAuthenticate from "vue-authenticate";
+import VueAxios from "vue-axios";
+import axios from "axios";
 
+Vue.use(VueAxios, axios);
 Vue.use(VueRouter);
+Vue.use(VueAuthenticate, {
+  tokenName: "access_token",
+  storageType: "cookieStorage",
+  providers: {
+    oauth2: {
+      authorizationEndpoint: process.env.VUE_APP_AUTH_ENDPOINT,
+      clientId: process.env.VUE_APP_CLIENT_ID,
+      responseType: "token"
+    }
+  }
+});
 
 const routes = [
   {
